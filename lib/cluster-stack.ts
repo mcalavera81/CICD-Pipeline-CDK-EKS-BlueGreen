4c938e5b3b3346da76f9208054e9a104df4357e8
Farid <software.development.9@olympicchannel.com>
2021-04-02 15:24:27 +0200

    uodate

--- lib/cluster-stack.ts
+++ lib/cluster-stack.ts
@@ -1,23 +1,68 @@
-import * as cdk from '@aws-cdk/core';
-import * as iam from '@aws-cdk/aws-iam';
-import * as eks from '@aws-cdk/aws-eks';
-import * as ec2 from '@aws-cdk/aws-ec2';
-import { PhysicalName } from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
+import * as eks from "@aws-cdk/aws-eks";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import { PhysicalName } from "@aws-cdk/core";
+
+export interface EksProps extends cdk.StackProps {
+  cluster: eks.Cluster;
+}
 
 export class ClusterStack extends cdk.Stack {
+  public readonly cluster: eks.Cluster;
+  public readonly firstRegionRole: iam.Role;
+  public readonly secondRegionRole: iam.Role;
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const primaryRegion = 'ap-northeast-2';
+    const primaryRegion = "ap-south-1";
 
+    const clusterAdmin = new iam.Role(this, "AdminRole", {
+      assumedBy: new iam.AccountRootPrincipal(),
+    });
+
+    const cluster = new eks.Cluster(this, "demogo-cluster", {
+      clusterName: `demogo`,
+      mastersRole: clusterAdmin,
+      version: eks.KubernetesVersion.V1_18,
+      defaultCapacity: 2,
+      defaultCapacityInstance:
+        cdk.Stack.of(this).region == primaryRegion
+          ? new ec2.InstanceType("r5.xlarge")
+          : new ec2.InstanceType("m5.2xlarge"),
+    });
+
+    cluster.addAutoScalingGroupCapacity("spot-group", {
+      instanceType: new ec2.InstanceType("m5.xlarge"),
+      spotPrice: cdk.Stack.of(this).region == primaryRegion ? "0.248" : "0.192",
+    });
+
+    if (cdk.Stack.of(this).region == primaryRegion) {
+      this.firstRegionRole = createDeployRole(this, `for-1st-region`, cluster);
+    } else {
+      this.secondRegionRole = createDeployRole(this, `for-2nd-region`, cluster);
+    }
+
+    this.cluster = cluster;
   }
 }
 
-function createDeployRole(scope: cdk.Construct, id: string, cluster: eks.Cluster): iam.Role {
+export interface CicdProps extends cdk.StackProps {
+  firstRegionCluster: eks.Cluster;
+  secondRegionCluster: eks.Cluster;
+  firstRegionRole: iam.Role;
+  secondRegionRole: iam.Role;
+}
+
+function createDeployRole(
+  scope: cdk.Construct,
+  id: string,
+  cluster: eks.Cluster
+): iam.Role {
   const role = new iam.Role(scope, id, {
     roleName: PhysicalName.GENERATE_IF_NEEDED,
-    assumedBy: new iam.AccountRootPrincipal()
+    assumedBy: new iam.AccountRootPrincipal(),
   });
   cluster.awsAuth.addMastersRole(role);
 
--- lib/container-stack.ts
+++ lib/container-stack.ts
@@ -1,13 +1,25 @@
-import * as cdk from '@aws-cdk/core';
-import { readYamlFromDir } from '../utils/read-file';
+import * as cdk from "@aws-cdk/core";
+import { readYamlFromDir } from "../utils/read-file";
+import { EksProps } from "./cluster-stack";
 
 export class ContainerStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: EksProps) {
     super(scope, id, props);
 
+    const cluster = props.cluster;
+    const commonFolder = "./yaml-common/";
+    const regionFolder = `./yaml-${cdk.Stack.of(this).region}/`;
 
+    readYamlFromDir(commonFolder, cluster);
+    readYamlFromDir(regionFolder, cluster);
+
+    cluster.addHelmChart(`flux`, {
+      repository: "https://charts.fluxcd.io",
+      chart: "flux",
+      release: "flux",
+      values: {
+        "git.url": "git@github.com:org/repo",
+      },
+    });
   }
-
 }
-
-
